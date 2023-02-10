b7eddf3f6e050e10ad86d04db2a46fe56a89fa89
albertobaggio96 <albertobaggio96@gmail.com>
2023-02-10 16:05:09 +0100

    create trains_table

---
+++ database/migrations/2023_02_10_144320_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string("azienda", 30);
+            $table->string("stazione_di_partenza", 30);
+            $table->dateTime("orario_di_partenza");
+            $table->dateTime("orairo_di_arrivo");
+            $table->string("codice_treno", 10);
+            $table->tinyInteger("numero_carrozze");
+            $table->boolean("in_orario");
+            $table->boolean("cancellato");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
