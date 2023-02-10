a03329ba4f3a306be2aad4b91772903238417893
albertobaggio96 <albertobaggio96@gmail.com>
2023-02-10 16:49:04 +0100

    update seedr to database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+use App\Models\Train;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,13 +14,19 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 0; $i < 10; $i++){
+            $newtrain = new Train();
+            $newtrain->azienda = $faker->name;
+            $newtrain->stazione_di_partenza = $faker->sentence(4, true);
+            $newtrain->orario_di_partenza = $faker->dateTimeBetween("-1 days", "+1 week");
+            $newtrain->orairo_di_arrivo = $faker->dateTimeBetween("-1 day", "+1 week");
+            $newtrain->codice_treno = $faker->bothify('????-####');
+            $newtrain->numero_carrozze = $faker->numberBetween(0, 100);
+            $newtrain->in_orario = $faker->boolean(80);
+            $newtrain->cancellato = $faker->boolean(5);
+            $newtrain->save();
+        }
     }
 }
